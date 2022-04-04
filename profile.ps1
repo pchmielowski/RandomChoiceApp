6e34ba0be597b54ad5051c7a8bccf8b1b2c4e64e
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2022-04-04 20:07:08 +0200

    Remove unnecessary files

--- profile.ps1
+++
@@ -1,7 +0,0 @@
-git checkout no_ksp
-./gradlew clean
-gradle-profiler --benchmark --iterations 100 --project-dir . --scenario-file profile-scenarios.txt
-
-git checkout main
-./gradlew clean
-gradle-profiler --benchmark --iterations 100 --project-dir . --scenario-file profile-scenarios.txt
