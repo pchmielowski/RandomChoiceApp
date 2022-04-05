b659631c58a7058b6cccce0d2b19bb9704867c49
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2022-04-05 21:05:24 +0200

    Use M3

--- app/src/main/java/net/chmielowski/randomchoice/ui/widgets/Divider.kt
+++ app/src/main/java/net/chmielowski/randomchoice/ui/widgets/Divider.kt
@@ -2,11 +2,15 @@
 
 package net.chmielowski.randomchoice.ui.widgets
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 internal fun Divider(modifier: Modifier = Modifier) {
-    // Current implementation of Material 3 divider does not use alpha.
-    androidx.compose.material.Divider(modifier = modifier)
+    // Alpha value copied from Material 2. Refactor after Material 3 Divider is updated to use alpha.
+    androidx.compose.material3.Divider(
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+        modifier = modifier,
+    )
 }
