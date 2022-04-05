3443866c2bf20893fff4a8f40d3b32f7b80f2abb
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2022-04-05 11:30:21 +0200

    Add parameter

--- app/src/main/java/net/chmielowski/randomchoice/ui/screen/component/TopBar.kt
+++ app/src/main/java/net/chmielowski/randomchoice/ui/screen/component/TopBar.kt
@@ -5,6 +5,7 @@ package net.chmielowski.randomchoice.ui.screen.component
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -12,10 +13,12 @@ internal fun TopBar(
     navigationIcon: @Composable () -> Unit = {},
     title: String,
     actions: @Composable (RowScope.() -> Unit) = {},
+    scrollBehavior: TopAppBarScrollBehavior? = null,
 ) {
     LargeTopAppBar(
         navigationIcon = navigationIcon,
         title = { Text(title) },
         actions = actions,
+        scrollBehavior = scrollBehavior,
     )
 }
