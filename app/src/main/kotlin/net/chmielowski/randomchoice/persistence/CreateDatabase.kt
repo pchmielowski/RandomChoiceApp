830f48aba32f566c91006c3d58ab1bc1fa8b9a8d
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2022-04-07 21:13:26 +0200

    Adapter

--- app/src/main/kotlin/net/chmielowski/randomchoice/persistence/CreateDatabase.kt
+++ app/src/main/kotlin/net/chmielowski/randomchoice/persistence/CreateDatabase.kt
@@ -6,6 +6,8 @@ import com.squareup.sqldelight.android.AndroidSqliteDriver
 import com.squareup.sqldelight.db.SqlDriver
 import net.chmielowski.randomchoice.Database
 import net.chmielowski.randomchoice.core.DilemmaId
+import net.chmielowski.randomchoice.core.Option
+import net.chmielowski.randomchoice.persistence.Option as DbOption
 
 internal fun createPersistentAndroidDatabase(context: Context) =
     createAndroidDatabase(context, "main.db")
@@ -18,9 +20,13 @@ internal fun createDatabase(driver: SqlDriver): Database {
         override fun decode(databaseValue: Long) = DilemmaId(databaseValue)
         override fun encode(value: DilemmaId) = value.value
     }
+    val optionAdapter = object : ColumnAdapter<Option, String> {
+        override fun decode(databaseValue: String) = Option(databaseValue)
+        override fun encode(value: Option) = value.text
+    }
     return Database(
         driver = driver,
         choiceAdapter = Choice.Adapter(dilemmaIdAdapter),
-        optionAdapter = Option.Adapter(dilemmaIdAdapter),
+        optionAdapter = DbOption.Adapter(optionAdapter, dilemmaIdAdapter),
     )
 }
