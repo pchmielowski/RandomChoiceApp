78fa15479ec89e60e9cb2b1808c61681aab5d676
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2022-04-10 12:22:12 +0200

    Allow making choice with photo (#33)
    * Keep byte array
    
    * Don't crash
    
    * Show chosen photo
    
    * Scale image
    
    * Padding
    
    * TODO
    
    * Detekt
    
    * Detekt

--- app/src/main/kotlin/net/chmielowski/randomchoice/ui/screen/result/ResultScreen.kt
+++ app/src/main/kotlin/net/chmielowski/randomchoice/ui/screen/result/ResultScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import net.chmielowski.randomchoice.R
-import net.chmielowski.randomchoice.core.MakingDecisionWithImagesNotSupportedException
 import net.chmielowski.randomchoice.core.Option
 import net.chmielowski.randomchoice.core.Result
 import net.chmielowski.randomchoice.ui.screen.component.AnimatedResult
@@ -53,7 +52,7 @@ internal fun ResultScreen(
                         text = AnnotatedString(
                             when (val chosen = result.chosen) {
                                 is Option.Text -> chosen.text
-                                is Option.Image -> throw MakingDecisionWithImagesNotSupportedException()
+                                is Option.Image -> ""
                             }
                         )
                     }
