9597ab088f897183186f75021bb6a54acd2f965f
Piotr Chmielowski <piotr.a.chmielowski@gmail.com>
2022-04-10 12:44:43 +0200

    Banner added

--- app/src/main/kotlin/net/chmielowski/randomchoice/ui/screen/input/InputScreen.kt
+++ app/src/main/kotlin/net/chmielowski/randomchoice/ui/screen/input/InputScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.material.icons.outlined.WbTwilight
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -187,6 +188,9 @@ internal fun InputScreen(
                 }
                 .padding(16.dp),
         ) {
+            if (state.mode == Mode.Image) {
+                PhotoModeBanner()
+            }
             OptionTextFields(
                 dilemma = state.dilemma,
                 onIntent = onIntent,
@@ -206,6 +210,22 @@ internal fun InputScreen(
             Spacer(modifier = Modifier.height(100.dp)) // Let the user scroll content up.
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PhotoModeBanner() {
+    ElevatedCard {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Image(Icons.Outlined.Info, contentDescription = null)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(R.string.message_photo_mode_experimental),
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+    Spacer(modifier = Modifier.height(18.dp))
 }
 
 @Suppress("LongParameterList")
